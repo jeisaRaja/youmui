ef305ca2ca9a0c83cd2674e9a6e41190e2ac70d5
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-10-20 18:15:38 +0700

    Refactor audio playback commands and handle termination gracefully
    - Add play/pause sign
    - Introduced a new method `terminatePreviousCommands` to kill existing yt-dlp and ffmpeg processes before starting new ones.
    - Added error handling to suppress specific error messages when processes are killed by signals.
    - Improved the `FetchAndPlayAudio` method by making it more structured and clear.
    - Introduced a helper function `isSignalKilled` to check for specific signal termination.

--- stream/stream.go
+++ stream/stream.go
@@ -13,11 +13,13 @@ import (
 )
 
 type Player struct {
-	ctrl     *beep.Ctrl
-	streamer beep.StreamSeekCloser
-	format   beep.Format
-	isPaused bool
-	volume   *effects.Volume
+	ctrl      *beep.Ctrl
+	streamer  beep.StreamSeekCloser
+	format    beep.Format
+	isPaused  bool
+	volume    *effects.Volume
+	ytdlpCmd  *exec.Cmd
+	ffmpegCmd *exec.Cmd
 }
 
 func NewPlayer() *Player {
@@ -32,43 +34,62 @@ func NewPlayer() *Player {
 	}
 }
 
+func (p *Player) terminatePreviousCommands() {
+	if p.ytdlpCmd != nil {
+		if err := p.ytdlpCmd.Process.Kill(); err != nil {
+			fmt.Printf("Failed to kill yt-dlp: %v\n", err)
+		}
+		if err := p.ytdlpCmd.Wait(); err != nil && isSignalKilled(err) {
+			fmt.Printf("yt-dlp command failed: %v\n", err)
+		}
+	}
+
+	if p.ffmpegCmd != nil {
+		if err := p.ffmpegCmd.Process.Kill(); err != nil {
+			fmt.Printf("Failed to kill ffmpeg: %v\n", err)
+		}
+		if err := p.ffmpegCmd.Wait(); err != nil && isSignalKilled(err) {
+			fmt.Printf("ffmpeg command failed: %v\n", err)
+		}
+	}
+}
+
 func (p *Player) FetchAndPlayAudio(url string) error {
+	done := make(chan bool)
+	p.terminatePreviousCommands()
+	p.ytdlpCmd = exec.Command("yt-dlp", "-f", "bestaudio", "-o", "-", url)
 
-	cmd := exec.Command("yt-dlp", "-f", "bestaudio", "-o", "-", url)
-
-	stdout, err := cmd.StdoutPipe()
+	stdout, err := p.ytdlpCmd.StdoutPipe()
 	if err != nil {
 		return fmt.Errorf("failed to get stdout from yt-dlp: %v", err)
 	}
 
-	if err := cmd.Start(); err != nil {
+	if err := p.ytdlpCmd.Start(); err != nil {
 		return fmt.Errorf("failed to run yt-dlp: %v", err)
 	}
 
-	convertCmd := exec.Command("ffmpeg", "-i", "pipe:0", "-acodec", "libmp3lame", "-f", "mp3", "pipe:1")
-	convertCmd.Stdin = stdout
-	convertedStdout, err := convertCmd.StdoutPipe()
+	p.ffmpegCmd = exec.Command("ffmpeg", "-i", "pipe:0", "-acodec", "libmp3lame", "-f", "mp3", "pipe:1")
+	p.ffmpegCmd.Stdin = stdout
+	convertedStdout, err := p.ffmpegCmd.StdoutPipe()
 	if err != nil {
 		return fmt.Errorf("failed to get stdout from ffmpeg: %v", err)
 	}
 
-	if err := convertCmd.Start(); err != nil {
+	if err := p.ffmpegCmd.Start(); err != nil {
 		return fmt.Errorf("failed to run ffmpeg: %v", err)
 	}
 
-	pipeReader, pipeWriter := io.Pipe()
-	defer pipeReader.Close()
-	defer pipeWriter.Close()
+	type teeReadCloser struct {
+		io.Reader
+		io.Closer
+	}
+	teeReader := io.TeeReader(convertedStdout, io.Discard)
+	readCloser := teeReadCloser{
+		Reader: teeReader,
+		Closer: convertedStdout,
+	}
 
-	go func() {
-		_, err := io.Copy(pipeWriter, convertedStdout)
-		if err != nil {
-			fmt.Printf("failed to copy ffmpeg output: %v", err)
-		}
-		pipeWriter.Close()
-	}()
-
-	streamer, format, err := mp3.Decode(pipeReader)
+	streamer, format, err := mp3.Decode(readCloser)
 
 	p.ctrl.Streamer = streamer
 	p.streamer = streamer
@@ -79,7 +100,6 @@ func (p *Player) FetchAndPlayAudio(url string) error {
 	defer streamer.Close()
 
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-	done := make(chan bool)
 	streamerWithCallback := beep.Callback(func() {
 		done <- true
 	})
@@ -89,11 +109,11 @@ func (p *Player) FetchAndPlayAudio(url string) error {
 	speaker.Play(beep.Seq(p.volume, streamerWithCallback))
 	<-done
 
-	if err := cmd.Wait(); err != nil {
+	if err := p.ytdlpCmd.Wait(); err != nil {
 		return fmt.Errorf("yt-dlp command failed: %v", err)
 	}
 
-	if err := convertCmd.Wait(); err != nil {
+	if err := p.ffmpegCmd.Wait(); err != nil {
 		return fmt.Errorf("ffmpeg command failed: %v", err)
 	}
 
@@ -116,4 +136,13 @@ func (p *Player) VolumeDown() {
 	speaker.Lock()
 	p.volume.Volume -= 0.5
 	speaker.Unlock()
+}
+
+func isSignalKilled(err error) bool {
+	if exitError, ok := err.(*exec.ExitError); ok {
+		if exitError.ProcessState.ExitCode() == 137 {
+			return true
+		}
+	}
+	return false
 }
