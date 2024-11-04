0b8422b01db1871b30005bf3a1dd43089a387b6e
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-11-04 14:11:02 +0700

    Optimize playlist fetching and parsing
    - Updated `fetchPlaylist` to return a slice of songs and an error instead of just an error.
    - Implemented yt-dlp's flat-playlist mode for efficient metadata retrieval from YouTube playlists.
    - Added `parsePlaylist` function to handle JSON unmarshalling of song data.
    - Expanded test coverage with `TestParsePlaylist` to validate playlist parsing.

--- api/playlist.go
+++ api/playlist.go
@@ -14,33 +14,38 @@ const (
 	SPOTIFY SourceType = "SPOTIFY"
 )
 
-func fetchPlaylist(url string, source SourceType) error {
-	var songs []Song
-
+// Uses yt-dlp’s flat-playlist mode to efficiently gather minimal metadata from YouTube playlists,
+// trading detailed info for speed and low resource use, as only basic song data is needed.
+func fetchPlaylist(url string, source SourceType) ([]Song, error) {
+	var output []byte
+	var err error
 	if source == YOUTUBE {
 		cmd := exec.Command("yt-dlp", "-j", "--flat-playlist", url)
-		output, err := cmd.Output()
+		output, err = cmd.Output()
+		fmt.Println(string(output))
 		if err != nil {
-			return err
+			return nil, err
 		}
-
-		// Split by newline
-		lines := strings.Split(string(output), "\n")
-		for _, line := range lines {
-			if line == "" {
-				continue
-			}
-			var song Song
-
-			if err := json.Unmarshal([]byte(line), &song); err != nil {
-				return fmt.Errorf("error unmarshalling song: %v", err)
-			}
-
-			songs = append(songs, song)
-		}
-
-		fmt.Printf("Songs are: %#v", songs)
 	}
 
-	return nil
+	return parsePlaylist(string(output))
+}
+
+func parsePlaylist(output string) ([]Song, error) {
+	var songs []Song
+	lines := strings.Split(output, "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		var song Song
+
+		if err := json.Unmarshal([]byte(line), &song); err != nil {
+			return nil, fmt.Errorf("error unmarshalling song: %v", err)
+		}
+
+		songs = append(songs, song)
+	}
+
+	return songs, nil
 }
--- api/playlist_test.go
+++ api/playlist_test.go
@@ -1,11 +1,39 @@
 package api
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestFetchPlaylist(t *testing.T) {
 	var examplePlaylist = "https://music.youtube.com/playlist?list=PLfsJhwuMhRrB71-Ad-kuiY3HkiYqoTlo_&si=lrVlA7zaWnzLjchm"
-	err := fetchPlaylist(examplePlaylist, YOUTUBE)
+	songs, err := fetchPlaylist(examplePlaylist, YOUTUBE)
 	if err != nil {
 		t.Fatalf("error when fetching playlist: %v", err)
+	}
+	fmt.Printf("%#v", songs)
+}
+
+func TestParsePlaylist(t *testing.T) {
+	mockOutput := ` {"title": "Song One", "url": "link1"}
+                  {"title": "Song Two", "url": "link2"}
+                  {"title": "Song Three", "url": "link3"}`
+
+	expectedSongs := []Song{
+		{Title: "Song One", URL: "link1"},
+		{Title: "Song Two", URL: "link2"},
+		{Title: "Song Three", URL: "link3"},
+	}
+
+	outSongs, err := parsePlaylist(mockOutput)
+	if err != nil {
+		t.Fatalf("failed to parse the playlist string: %v", err)
+	}
+
+	for i := range expectedSongs {
+		if !reflect.DeepEqual(expectedSongs[i], outSongs[i]) {
+			t.Fatalf("song and expected song are different: expected %v, got %v", expectedSongs[i], outSongs[i])
+		}
 	}
 }
